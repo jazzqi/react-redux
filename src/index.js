316278bfb0ca888da732821dafa98f941f116b29
qfei <jazzqi@gmail.com>
2018-09-14 20:32:47 +0800

    config thunk middleware

--- src/index.js
+++ src/index.js
@@ -4,17 +4,23 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-import { combineReducers, createStore } from "redux";
+import thunk from 'redux-thunk';
+import { applyMiddleware, compose, combineReducers, createStore } from "redux";
 // provider give us access to the store
 import { Provider } from "react-redux";
 import productReducer from "./reducers/product-reducer";
 import userReducer from "./reducers/user-reducer";
 
-
 const allReducers = combineReducers({
   products: productReducer,
   user: userReducer
 });
+
+const allStoreEnhancers = compose(
+  // the thunk middleware needs to be come first or else it won't work. 
+  applyMiddleware(thunk),
+  window.devToolsExtension && window.devToolsExtension()
+);
 
 const store = createStore(
   allReducers,
@@ -25,13 +31,12 @@ const store = createStore(
       }
     ],
     user: "Steve"
-  },
-  window.devToolsExtension && window.devToolsExtension()
+  } // , window.devToolsExtension && window.devToolsExtension()
 );
 
 ReactDOM.render(
   <Provider store={store}>
-    <App aRandomProps='whatever' />
+    <App aRandomProps="whatever" />
   </Provider>,
   document.getElementById("root")
 );
