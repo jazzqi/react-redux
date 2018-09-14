d09eab35c6512a18c68b737538fd6cbc41015cab
qfei <jazzqi@gmail.com>
2018-09-14 17:45:17 +0800

    use connect, mapStateToProps and mapActionToProps to decouple the actions and reducers.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { connect } from "react-redux";
+import { updateUser } from "./actions/user-actions";
 
 class App extends Component {
+  // In order to use this, we need to add a constructor and bind this
+  constructor(props) {
+    super(props);
+    this.onUpdateUser = this.onUpdateUser.bind(this);
+  }
+
+  onUpdateUser() {
+    this.props.onUpdateUser("Bill");
+  }
+
   render() {
+    console.log(this.props);
     return (
       <div className="App">
         <header className="App-header">
@@ -13,9 +27,20 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div onClick={this.onUpdateUser}> Update user </div>
+        {this.props.user}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  products: state.products,
+  user: state.user
+});
+
+const mapActionToProps = {
+  onUpdateUser: updateUser
+};
+
+export default connect(mapStateToProps, mapActionToProps)(App);
